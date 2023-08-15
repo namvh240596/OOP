19ef59e1a05319d53ef7944f1a584641e1d22fd6
namvh <namvh240596@gmail.com>
2023-08-15 14:12:30 +0700

    Chỉnh bài

--- lib/SinhVien.dart
+++ lib/SinhVien.dart
@@ -1,34 +1,41 @@
 import 'dart:io';
 
-class SinhVien{
-
-  String ten='';
-  int tuoi= 0;
-  double diemtrungbinh=0.0 ;
-  String chuyenganh='';
-  void input(){
-    stdout.write('Tên:');
-    ten = stdin.readLineSync()!;
-
-    stdout.write('Tuổi:');
-    tuoi= int.parse(stdin.readLineSync()!);
-
-    stdout.write('Điểm trunh bình:');
-    diemtrungbinh=double.parse(stdin.readLineSync()!);
-
-    stdout.write('Chuyên Ngành:');
-    chuyenganh=stdin.readLineSync()!;
-  }
-  void ouput(){
-    print('ten: $ten');
-    print('tuoi: $tuoi');
-    print('diemtrungbinh: $diemtrungbinh');
-    print('chuyennganh: $chuyenganh');
-  }
-}
-void main(){
-  SinhVien sinhvien =SinhVien();
-  sinhvien.input();
-  sinhvien.ouput();
+void main() {
+  var sinhvien=SinhVien.empty();
+    sinhvien.nhap();
+    sinhvien.xuat();
 }
 
+class SinhVien {
+  //properties thuộc tính
+  String? ten;
+  int? tuoi;
+  double? diem;
+  String? chuyennganh;
+
+  // Constructor
+  SinhVien(this.ten, this.tuoi, this.diem, this.chuyennganh);
+
+  SinhVien.empty();
+
+  //methods hành động
+  void nhap() {
+    print('ten:');
+    this.ten = stdin.readLineSync() ?? '';
+
+    print('tuoi');
+    this.tuoi = int.parse(stdin.readLineSync() ?? '');
+
+    print('diem');
+    this.diem = double.parse(stdin.readLineSync() ?? '');
+
+    print('chuyen nganh');
+    this.chuyennganh = stdin.readLineSync() ?? '';
+  }
+  void xuat(){
+    print('Tên: ${this.ten}');
+    print('Tuổi: ${this.tuoi}');
+    print('Điểm: ${this.diem}');
+    print('Chuyên Ngành: ${this.chuyennganh}');
+  }
+}
