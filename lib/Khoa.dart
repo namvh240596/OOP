a23f7e50e5753ecac775942f28557d8d056a1860
namvh <namvh240596@gmail.com>
2023-08-15 21:20:18 +0700

    Chỉnh bài

--- lib/Khoa.dart
+++ lib/Khoa.dart
@@ -1,3 +1,24 @@
-class Khoa{
+void main() {
+  Khoa khoa = Khoa(khoa: 'Sư Phạm', danhsach:''
+      'Tiếng Việt ,'
+      ' Toán ,'
+      'Lý ,'
+      'Hóa,'
+      '...');
+  khoa.xuat();
+}
 
+class Khoa {
+  //properties thuộc tính
+  String? khoa;
+  String? danhsach;
+
+// Constructor
+  Khoa({this.khoa, this.danhsach});
+
+//methods hành động
+  void xuat() {
+    print('Khoa:$khoa');
+    print('Danh sách:$danhsach');
+  }
 }
